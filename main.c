a0fddc49a83ee698eaf317086ebcd2cc117275ed
Trent <trent.gill@gmail.com>
2019-05-18 22:24:11 -0400

    cleanup main.c

--- main.c
+++ main.c
@@ -1,8 +1,3 @@
-//#include "lib/debug_usart.h"
-//#include "lib/f303_dac.h"
-//#include "lib/dream.h"
-//#include "lib/STP16_led.h"
-
 #include "lib/system.h"
 #include "lib/dout.h"
 
@@ -13,7 +8,7 @@ int main(void)
 
     dout_t led = dout_init();
 
-	while (1){
+    while (1){
         dout_set(&led,1);
-	}
+    }
 }
